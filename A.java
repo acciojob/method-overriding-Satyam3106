a194be87164194d4e91548f80c3202448389fe6f
Satyam3106 <148087403+Satyam3106@users.noreply.github.com>
2024-02-11 09:18:37 +0530

    Add files via upload

---
+++ A.java
@@ -0,0 +1,17 @@
+public class A {
+
+   String meth() {
+       return "Invoking method from class A";
+   }
+
+}
+
+class B extends A {
+
+    public static void main(String[] args)
+    {
+        B obj = new B();
+        System.out.println(obj.meth());
+    }
+}
+
